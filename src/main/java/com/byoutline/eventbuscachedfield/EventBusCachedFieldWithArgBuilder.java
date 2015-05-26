f749a8df9ed477018c1143fcc518e870fddc725d
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-05-26 15:28:31 +0200

    Added: Initial commit that contains fork of OttoCachedField with Otto replaced with EventBus.

---
+++ src/main/java/com/byoutline/eventbuscachedfield/EventBusCachedFieldWithArgBuilder.java
@@ -0,0 +1,105 @@
+package com.byoutline.eventbuscachedfield;
+
+import com.byoutline.cachedfield.ProviderWithArg;
+import com.byoutline.eventbuscachedfield.internal.EventIBus;
+import com.byoutline.ibuscachedfield.events.ResponseEventWithArg;
+import de.greenrobot.event.EventBus;
+
+import javax.annotation.Nullable;
+import javax.inject.Provider;
+
+/**
+ * Fluent interface builder of {@link EventBusCachedField}. If you do not like
+ * fluent interface create {@link EventBusCachedField} by one of its constructors.
+ *
+ * @param <RETURN_TYPE> Type of object to be cached.
+ * @author Sebastian Kacprzak <sebastian.kacprzak at byoutline.com>
+ */
+public class EventBusCachedFieldWithArgBuilder<RETURN_TYPE, ARG_TYPE> {
+
+    private ProviderWithArg<RETURN_TYPE, ARG_TYPE> valueGetter;
+    private ResponseEventWithArg<RETURN_TYPE, ARG_TYPE> successEvent;
+    private ResponseEventWithArg<Exception, ARG_TYPE> errorEvent;
+    private Provider<String> sessionIdProvider;
+    private EventBus bus;
+
+    public EventBusCachedFieldWithArgBuilder() {
+        bus = EventBusCachedField.defaultBus;
+        sessionIdProvider = EventBusCachedField.defaultSessionIdProvider;
+    }
+
+    public SuccessEvent withValueProvider(ProviderWithArg<RETURN_TYPE, ARG_TYPE> valueProvider) {
+        this.valueGetter = valueProvider;
+        return new SuccessEvent();
+    }
+
+    public class SuccessEvent {
+
+        private SuccessEvent() {
+        }
+
+        public ErrorEventSetter withSuccessEvent(ResponseEventWithArg<RETURN_TYPE, ARG_TYPE> successEvent) {
+            EventBusCachedFieldWithArgBuilder.this.successEvent = successEvent;
+            return new ErrorEventSetter();
+        }
+    }
+
+    public class ErrorEventSetter {
+
+        private ErrorEventSetter() {
+        }
+
+        public CustomSessionIdProvider withResponseErrorEvent(@Nullable ResponseEventWithArg<Exception, ARG_TYPE> errorEvent) {
+            EventBusCachedFieldWithArgBuilder.this.errorEvent = errorEvent;
+            return new CustomSessionIdProvider();
+        }
+
+        public EventBusCachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
+            return EventBusCachedFieldWithArgBuilder.this.build();
+        }
+    }
+
+    public class CustomSessionIdProvider {
+
+        private CustomSessionIdProvider() {
+        }
+
+        public CustomBus withCustomSessionIdProvider(Provider<String> sessionIdProvider) {
+            EventBusCachedFieldWithArgBuilder.this.sessionIdProvider = sessionIdProvider;
+            return new CustomBus();
+        }
+
+        public EventBusCachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
+            return EventBusCachedFieldWithArgBuilder.this.build();
+        }
+    }
+
+    public class CustomBus {
+
+        private CustomBus() {
+        }
+
+        public Builder withCustomBus(EventBus bus) {
+            EventBusCachedFieldWithArgBuilder.this.bus = bus;
+            return new Builder();
+        }
+
+        public EventBusCachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
+            return EventBusCachedFieldWithArgBuilder.this.build();
+        }
+    }
+
+    public class Builder {
+
+        private Builder() {
+        }
+
+        public EventBusCachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
+            return EventBusCachedFieldWithArgBuilder.this.build();
+        }
+    }
+
+    private EventBusCachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
+        return new EventBusCachedFieldWithArg<RETURN_TYPE, ARG_TYPE>(sessionIdProvider, valueGetter, successEvent, errorEvent, new EventIBus(bus));
+    }
+}
