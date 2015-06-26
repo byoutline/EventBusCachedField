f57b29249300656e2c2fdd198f5120975a64b4cc
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-06-26 15:58:18 +0200

    Added: tests for passing executors for builder with args.

--- src/test/groovy/com/byoutline/eventbuscachedfield/CustomExecutorsSpec.groovy
+++ src/test/groovy/com/byoutline/eventbuscachedfield/CustomExecutorsSpec.groovy
@@ -2,6 +2,8 @@ package com.byoutline.eventbuscachedfield
 
 import com.byoutline.eventcallback.ResponseEvent
 import com.byoutline.eventcallback.ResponseEventImpl
+import com.byoutline.ibuscachedfield.events.ResponseEventWithArg
+import com.byoutline.ibuscachedfield.events.ResponseEventWithArgImpl
 import com.google.common.util.concurrent.MoreExecutors
 import de.greenrobot.event.EventBus
 import spock.lang.Shared
@@ -17,21 +19,20 @@ import java.util.concurrent.FutureTask
 class CustomExecutorsSpec extends spock.lang.Specification {
     @Shared
     String value = "value"
-    ResponseEvent<String> successEvent
-    ResponseEvent<Exception> errorEvent
+    @Shared
+    Map<Integer, String> argToValueMap = [1: 'a', 2: 'b']
     EventBus bus
 
     def setup() {
         bus = Mock()
-        successEvent = new ResponseEventImpl<>()
-        errorEvent = new ResponseEventImpl<>()
-
         EventBusCachedField.init(MockFactory.getSameSessionIdProvider(), bus)
     }
 
-    def "should use passed executor for loading data"() {
+    def "should use passed executor for loading data no arg"() {
         given:
         boolean called = false
+        ResponseEvent<String> successEvent = new ResponseEventImpl<>()
+        ResponseEvent<Exception>errorEvent = new ResponseEventImpl<>()
         ExecutorService executor = [
                 submit: { called = true; return new FutureTask((Runnable) it, null); }
         ] as ExecutorService
@@ -49,14 +50,60 @@ class CustomExecutorsSpec extends spock.lang.Specification {
         called
     }
 
-    def "should use passed executor for state listener"() {
+    def "should use passed executor for state listener no arg"() {
         given:
         boolean called = false
+        ResponseEvent<String> successEvent = new ResponseEventImpl<>()
+        ResponseEvent<Exception>errorEvent = new ResponseEventImpl<>()
         Executor stateListenersExecutor = { called = true; it.run() } as Executor
         EventBusCachedField field = EventBusCachedField.builder()
                 .withValueProvider(MockFactory.getStringGetter(value))
                 .withSuccessEvent(successEvent)
                 .withGenericErrorEvent(errorEvent)
+                .withCustomValueGetterExecutor(MoreExecutors.newDirectExecutorService())
+                .withCustomStateListenerExecutor(stateListenersExecutor)
+                .build();
+
+        when:
+        field.postValue()
+
+        then:
+        called
+    }
+
+
+    def "should use passed executor for loading data with arg"() {
+        given:
+        boolean called = false
+        ResponseEventWithArg<String, Integer> successEvent = new ResponseEventWithArgImpl<>()
+        ResponseEventWithArg<Exception, Integer> errorEvent = new ResponseEventWithArgImpl<>()
+        ExecutorService executor = [
+                submit: { called = true; return new FutureTask((Runnable) it, null); }
+        ] as ExecutorService
+        EventBusCachedFieldWithArg field = EventBusCachedFieldWithArg.builder()
+                .withValueProvider(MockFactory.getStringGetter(argToValueMap))
+                .withSuccessEvent(successEvent)
+                .withResponseErrorEvent(errorEvent)
+                .withCustomValueGetterExecutor(executor)
+                .build();
+
+        when:
+        field.postValue()
+
+        then:
+        called
+    }
+
+    def "should use passed executor for state listener with arg"() {
+        given:
+        boolean called = false
+        ResponseEventWithArg<String, Integer> successEvent = new ResponseEventWithArgImpl<>()
+        ResponseEventWithArg<Exception, Integer> errorEvent = new ResponseEventWithArgImpl<>()
+        Executor stateListenersExecutor = { called = true; it.run() } as Executor
+        EventBusCachedFieldWithArg field = EventBusCachedFieldWithArg.builder()
+                .withValueProvider(MockFactory.getStringGetter(argToValueMap))
+                .withSuccessEvent(successEvent)
+                .withResponseErrorEvent(errorEvent)
                 .withCustomValueGetterExecutor(MoreExecutors.newDirectExecutorService())
                 .withCustomStateListenerExecutor(stateListenersExecutor)
                 .build();
